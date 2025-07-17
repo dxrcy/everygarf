cddf2d218b04a8525a9aefb00b8be681dd311944
darcy <acednes@gmail.com>
2025-07-17 18:18:24 +1000

    refactor: remove `date_month_to_string` function

--- src/dates.rs
+++ src/dates.rs
@@ -1,23 +1,4 @@
-use chrono::{Datelike, Duration, NaiveDate, NaiveTime, Utc};
-
-//TODO: Documention
-pub fn date_month_to_string(date: NaiveDate) -> &'static str {
-    match date.month0() {
-        0 => "January",
-        1 => "February",
-        2 => "March",
-        3 => "April",
-        4 => "May",
-        5 => "June",
-        6 => "July",
-        7 => "August",
-        8 => "September",
-        9 => "October",
-        10 => "November",
-        11 => "December",
-        _ => unreachable!(),
-    }
-}
+use chrono::{Duration, NaiveDate, NaiveTime, Utc};
 
 pub fn first() -> NaiveDate {
     NaiveDate::from_ymd_opt(1978, 6, 19)
